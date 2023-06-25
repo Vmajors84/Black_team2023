a392a66da19b42e1812697b728473e5cf17b525d
Vmajors84 <131828554+Vmajors84@users.noreply.github.com>
2023-06-25 13:05:46 -0600

    Print text from exercise 4
     This was the output:
    
    ```
    C:\Users\Redsl\PycharmProjects\Black_team2023\venv\Scripts\python.exe C:\Users\Redsl\PycharmProjects\Black_team2023\Python_exersise_4.py
    How many hours did you work last month? 96.5
    What is your hourly rate? 53.8
    Last month, you earned 5191.7 dollars
    
    Process finished with exit code 0
    
    ```

--- Python_exersise_4.py
+++ Python_exersise_4.py
@@ -1,5 +1,4 @@
 hours = float(input("How many hours did you work last month? "))
 hourly_rate = float(input("What is your hourly rate? "))
 
-
-print("Last month, you earned {} dollars" .format(hours*hourly_rate))
+print("Last month, you earned {} dollars".format(hours * hourly_rate))
