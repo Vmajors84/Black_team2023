e9158df78a9442595c7ea645dc135d512f91b370
Vmajors84 <131828554+Vmajors84@users.noreply.github.com>
2023-06-25 11:53:23 -0600

    Print text from exercise 2
     This was the output:
    
    ```
    C:\Users\Redsl\PycharmProjects\Black_team2023\venv\Scripts\python.exe C:\Users\Redsl\PycharmProjects\Black_team2023\Python_exercise_2.py
    Your income is 250000 and you would pay 12500.0 income tax in Lowtaxland or 107500.0 income tax in Ripoffland. You would save 95000.0 by paying taxes in Lowtaxland!
    
    Process finished with exit code 0
    
    ```

---
+++ Python_exercise_2.py
@@ -0,0 +1,8 @@
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+lowtaxland_amt = income*lowtaxland_rate
+ripoffland_amt = income*ripoffland_rate
+
+
+print('Your income is {} and you would pay {} income tax in Lowtaxland or {} income tax in Ripoffland. You would save {} by paying taxes in Lowtaxland!'.format (income, lowtaxland_amt, ripoffland_amt, ripoffland_amt - lowtaxland_amt))
