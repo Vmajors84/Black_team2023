ff21ddbf9cd8014f1c65c42b3f4150e7f53d19d2
Vmajors84 <131828554+Vmajors84@users.noreply.github.com>
2023-06-27 17:32:50 -0600

    Print text from if practice codes
    This was the output:
    ```
    Do you like ice cream? y/n: y
    And do you like chocolate? y/n: y
    Excellent! You can win a years supply of Dairy Queen ice cream!
    
    Process finished with exit code 0
    
    Do you like ice cream? y/n: y
    And do you like chocolate? y/n: n
    sorry to hear that.. =(
    
    Process finished with exit code 0
    
    Do you like ice cream? y/n: n
    sorry to hear that =(
    
    Process finished with exit code 0
    
    ```

---
+++ else_practice.py
@@ -0,0 +1,9 @@
+answer_a = input('Do you like ice cream? y/n: ')
+if answer_a == 'y':
+    answer_b = input('And do you like chocolate? y/n: ')
+    if answer_b == 'y':
+        print('Excellent! You can win a years supply of Dairy Queen ice cream!')
+    else:
+        print('sorry to hear that.. =(')
+else:
+    print('sorry to hear that =(')
